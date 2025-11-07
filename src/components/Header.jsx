2f5bfdcb5a12129fceea78db98e349fc69ec6000
darwin <dr.gamez30@ciaf.edu.co>
2025-11-06 22:09:02 -0500

    Se agregaron todos los archivos

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+export const Header = () => {
+    const title = "GifsApp"
+
+  return <>
+    <h1>{title}</h1>
+  </>  
+}
---
+++ src/gifs/CurrentsGifs.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const CurrentsGifs = () => {
+    return (
+        <div>CurrentsGifs</div>
+    );
+}
